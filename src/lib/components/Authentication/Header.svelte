7f713e725995c245f1cae72bbb4ef4734fb9230d
StefanoLonghena <70469792+StefanoLonghena@users.noreply.github.com>
2025-03-25 11:34:42 +0100

    finished login page frontend

--- src/lib/components/Authentication/Header.svelte
+++ src/lib/components/Authentication/Header.svelte
@@ -7,12 +7,12 @@
 
 <div class="flex flex-col">
 	<div class="flex items-center justify-center">
-		<BookOpenText class="mx-1 size-10 text-accent content-center"/>
-		<h1 class="text-3xl font-semibold text-primary">Shelfy</h1>
+		<BookOpenText class="mx-1 size-10 text-primary content-center"/>
+		<h1 class="text-3xl font-semibold text-black">Shelfy</h1>
 	</div>
 	
 	<div class="flex flex-col items-center m-5">
 		<h3 class="font-medium text-2xl text-black">{title}</h3>
-		<p class="font-light text-muted-foreground">{greeting}</p>
+		<p class="font-light text-gray-500">{greeting}</p>
 	</div>
 </div>
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
-	import { Header } from "$components"
+	import { Header } from '$components';
 </script>
 
 <div class="card w-150">
 	<Header title="Welcome Back" greeting="Sign in to your account"></Header>
-	<form action="POST" class="flex flex-col w-full items-center">
-		<div class="flex flex-col w-2/3">
+	<form action="POST" class="flex w-full flex-col items-center">
+		<div class="flex w-5/6 flex-col">
 			<label for="fname" class="font-medium">Email</label>
-			<input type="text" name="email" id="email" class="border-2 border-black rounded-md">
+			<input
+				type="text"
+				name="email"
+				id="email"
+				placeholder="your.email@email.com"
+				class="rounded-md border-2 border-black my-2"
+			/>
+			<label for="pass" class="font-medium">Password</label>
+			<input
+				type="text"
+				name="password"
+				id="pass"
+				placeholder="your.email@email.com"
+				class="rounded-md border-2 border-black my-2"
+			/>
 		</div>
+
+		<input type="submit" name="go" value="Submit" class="button-primary mt-10"/>
 	</form>
 </div>
