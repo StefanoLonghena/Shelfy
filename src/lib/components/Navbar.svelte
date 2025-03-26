c8a203e29f7c8a76b9be98533cc0864fe1a1015e
Stefano Longhena <70469792+StefanoLonghena@users.noreply.github.com>
2025-03-26 23:05:19 +0100

    started navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,3 +1,16 @@
-<div class="">
-	
+<script lang="ts">
+	import { BookOpenText } from "@lucide/svelte";
+</script>
+
+<div class="w-full h-[15%] py-[1%] px-[1.5%] bg-background-accent flex justify-between">
+	<div class="flex items-center justify-center">
+		<BookOpenText class="mx-1 size-10 text-primary content-center"/>
+		<h1 class="text-3xl font-semibold text-black">Shelfy</h1>
+	</div>
+	<div>
+		cazzi
+	</div>
+	<div>
+		cazzi
+	</div>
 </div>
--- src/routes/(protected)/dashboard/+page.svelte
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { Navbar } from "$components"
 	import { default as SignOutButton } from "$components/Authentication/SignOutButton.svelte"
 </script>
 
 
-
+<Navbar />
 
 <!-- Temporary Button to be moved in settings -->
 <SignOutButton />
