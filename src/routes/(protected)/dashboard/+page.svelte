f48ead449e796dfe8dbca87a91784578a80569d2
Stefano Longhena <70469792+StefanoLonghena@users.noreply.github.com>
2025-03-27 20:09:22 +0100

    started on main dashboard

--- src/routes/(protected)/dashboard/+page.svelte
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -2,6 +2,11 @@
 	import { default as SignOutButton } from "$components/Authentication/SignOutButton.svelte"
 </script>
 
+
+<section class="my-5">
+	<h1 class="text-primary font-semibold font-sans text-4xl">Welcome Back!</h1>
+	<p class="font-light text-gray-500">Here is an overview of your reading journey</p>
+</section>
 <!-- Temporary Button to be moved in settings 
 <SignOutButton>Prova</SignOutButton>
 -->
